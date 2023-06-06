c4bb3fa0f08dc0839ced018635c5ecf75c17c62c
Amanda Meneguini <admcmarshmallow@gmail.com>
2023-06-06 16:08:21 -0300

    edit modal

--- switzerland_bank/src/main/resources/static/scripts/account.js
+++ switzerland_bank/src/main/resources/static/scripts/account.js
@@ -1,8 +1,10 @@
+//constantes Deletar
 const buttonDeletar = document.getElementById('deletar');
 const modalDeletar = document.getElementById('modalDeletar');
 const cancelButton = document.getElementById('cancelar');
 const confirmButton = document.getElementById('confirmar');
 
+//Modal Deletar
 buttonDeletar.onclick = function () {
   modalDeletar.showModal();
 }
@@ -11,12 +13,18 @@ cancelButton.onclick = function () {
   modalDeletar.close();
 }
 
-// function exibirAviso() {
-//   var aviso = document.getElementById("modalDeletar");
-//   aviso.classList.remove("escondido");
-// }
+//constantes Editar
+const buttonEditar = document.getElementById ('editar');
+const modalEditar = document.getElementById('modalEditar');
+const cancelEditButton = document.getElementById('EditCancelar');
+const saveButton = document.getElementById('salvar');
 
-// function fecharAviso() {
-//   var aviso = document.getElementById("modalDeletar");
-//   aviso.classList.add("escondido");
-// }
+//Modal Editar
+
+buttonEditar.onclick = function () {
+  modalEditar.showModal();
+}
+
+cancelEditButton.onclick = function () {
+  modalEditar.close();
+}
