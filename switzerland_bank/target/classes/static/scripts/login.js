b0e293dbf3b36d07273c10731ed9a6c923709954
Amanda Meneguini <admcmarshmallow@gmail.com>
2023-06-07 18:00:01 -0300

    Modal Esqueci a Senha (Part I)

--- switzerland_bank/target/classes/static/scripts/login.js
+++ switzerland_bank/target/classes/static/scripts/login.js
@@ -10,3 +10,19 @@ olhoMostrar.onclick = () => {
     senha.type = "password";
   }
 };
+
+
+//Constantes Esqueci a senha
+const linkEsqueciSenha = document.getElementById('esqueci-senha');
+const modalEsqueciSenha = document.getElementById('modalEsqueciSenha');
+const cancelButton = document.getElementById('EditCancelar');
+const confirmButton = document.getElementById('confirmar');
+
+//Modal Esqueci a senha
+linkEsqueciSenha.onclick = function () {
+  modalEsqueciSenha.showModal();
+}
+
+cancelButton.onclick = function () {
+  modalEsqueciSenha.close();
+}
