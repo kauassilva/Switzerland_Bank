7ef06dd884620a5c8cc03c9b0e93bbbde677ab24
Breno Luiz <brenofialho36@gmail.com>
2023-06-11 16:53:31 -0300

    cartão front adicionado

--- switzerland_bank/src/main/resources/static/scripts/cartoes.js
+++
@@ -1,9 +0,0 @@
-const cartaoVerifier = document.getElementById('cartaoNumero')
-
-function cartaoVerificar() {
-    if ("${client.cartao}" != null) {
-        cartaoVerificar.style.display = 'none';        
-    }else{
-    cartaoVerificar.display = 'block';
-    }
-}
